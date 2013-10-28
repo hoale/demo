198a5b2c28c23c1243de94abe8235fed693487c2
Hoa Le <hoale1402@gmail.com>
2013-10-29 04:16:04 +0700

    contact form validation

---
+++ js/contact.js
@@ -0,0 +1,38 @@
+$(function(){
+	var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	$('#contact-form').submit(function(){
+		var isValid = true;
+
+		var name = $("#name").val();
+		if ($.trim(name).length < 1) {
+			isValid = false;
+			$('#name-required-error').removeClass('hide');
+		} else {
+			$('#name-required-error').addClass('hide');
+		}
+
+		var email = $("#email").val();
+		if ($.trim(email).length < 1) {
+			isValid = false;
+			$('#email-required-error').removeClass('hide');
+		} else if (!emailRegex.test(email)) {
+			isValid = false;
+			$('#email-valid-error').removeClass('hide');
+		} else {
+			$('#email-required-error').addClass('hide');
+			$('#email-valid-error').removeClass('hide');
+		}
+
+		var message = $("#message").val();
+		if ($.trim(message).length < 1) {
+			isValid = false;
+			$('#message-required-error').removeClass('hide');
+		} else {
+			$('#message-required-error').addClass('hide');
+		}
+
+		console.info(email);
+		return isValid;
+
+	});
+});
